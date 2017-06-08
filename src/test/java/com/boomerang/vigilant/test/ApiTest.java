94adf70585816eafd86d2e63e944c1c475136ff0
Kevin Caravaggio <kevvurs@Galago686>
2017-06-07 20:42:47 -0700

    Cloud SQL test PASSING

--- src/test/java/com/boomerang/vigilant/test/ApiTest.java
+++ src/test/java/com/boomerang/vigilant/test/ApiTest.java
@@ -5,6 +5,9 @@ import com.boomerang.vigilant.service.VigilantService;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.ResultSetExtractor;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.testng.Assert;
@@ -15,6 +18,8 @@ import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -25,8 +30,11 @@ public class ApiTest extends AbstractTestNGSpringContextTests {
     @Autowired
     VigilantService vigilantService;
 
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
     @Test(
-            groups = {"universal"},
+            groups = {"off"},
             priority = 1
     )
     public void testEndpoints() {
@@ -60,5 +68,28 @@ public class ApiTest extends AbstractTestNGSpringContextTests {
         Rss rss = (Rss) u.unmarshal(url);
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
         LOG.info(gson.toJson(rss));
+    }
+
+    @Test(
+            groups = {"central"},
+            priority = 3
+    )
+    public void dbConn() {
+        LOG.info("Connecting to database");
+        final StringBuilder buil = new StringBuilder()
+                .append("\nSHOW TABLES:");
+        String tables =
+            jdbcTemplate.query("SHOW TABLES", new ResultSetExtractor<String>() {
+                @Override
+                public String extractData(ResultSet resultSet) throws SQLException, DataAccessException {
+
+                    while (resultSet.next()) {
+                        buil.append('\n').append(resultSet.getString(1));
+                    }
+                    return buil.toString();
+                }
+            });
+        Assert.assertEquals(tables.isEmpty(),false);
+        LOG.info(tables);
     }
 }
